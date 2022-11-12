f197c0fa7a58e5f1a3a10db2df3405ef8f46ef25
Brian Lara <Briandlara1@gmail.com>
2022-11-12 16:21:47 -0500

    added registerUser function

--- services/Auth.js
+++ services/Auth.js
@@ -9,3 +9,12 @@ export const SignInUser = async (data) => {
     throw error
   }
 }
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/users/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
