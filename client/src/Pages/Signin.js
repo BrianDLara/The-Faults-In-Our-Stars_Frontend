2af572177b8d009d910aed258e7be0cc8c63edba
Brian Lara <Briandlara1@gmail.com>
2022-11-12 16:48:42 -0500

    added functions to signIn page

--- client/src/Pages/Signin.js
+++ client/src/Pages/Signin.js
@@ -3,7 +3,19 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { SignInUser } from '../../../services/Auth'
 
-const Signin = () => {
+const Signin = ({ toggleAuthenticated, setUser }) => {
+  const navigate = useNavigate()
+  const [formValues, setFormValues] = useState({ username: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    toggleAuthenticated(true)
+    navigate('')
+  }
   return <div>Signin</div>
 }
 
