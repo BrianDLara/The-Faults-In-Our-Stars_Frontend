c07720af86858d17c01ab30ac39171e131ec730a
Brian Lara <Briandlara1@gmail.com>
2022-11-14 17:08:45 -0500

    checking for re-seeding

--- client/src/components/Zodiac.jsx
+++ client/src/components/Zodiac.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Zodiac = ({key, zodiacName, image, description }) => {
+const Zodiac = ({id, zodiacName, image, description }) => {
   return (
-    <div className='zodiac-container' key={key}>
+    <div className='zodiac-container' key={id}>
       <img src={image} alt={`of ${zodiacName}`}  id="zodiac-pic"/> 
       <h2 id="zodiac-name">{zodiacName}</h2> 
     </div>
