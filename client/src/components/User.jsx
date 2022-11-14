4889cb65285c74663acd2e0bf2f6b657d9fe2122
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-11-14 17:18:23 -0500

    merge

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const User = ({key, firstName, lastName, zodiacName, profilePic}) => {
+const User = ({id, firstName, lastName, zodiacName, profilePic}) => {
   return (
-    <div className='user-container' key={key}>
+    <div className='user-container' key={id}>
       <img src={profilePic} alt={`of ${firstName} ${lastName}`}  id="user-profilePic"/>
       {/* <span className='user-txt-container'>  */}
       <h2 id="user-name">{firstName} {lastName}&nbsp;</h2>
