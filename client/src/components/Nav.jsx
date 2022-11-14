5d76beda31b1ae83efd641fc8b8007b87d75d15d
Brian Lara <Briandlara1@gmail.com>
2022-11-14 15:14:31 -0500

    added navbar logic with a turnary function

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,28 +1,51 @@
 import { NavLink, Link} from 'react-router-dom'
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({ authenticated, user, handleLogOut }) => {
+  let authenticatedOptions
+  if (user) {
+    authenticatedOptions = (
+      <nav>
+        <h3>Welcome {user.firstName}!</h3>
+        <NavLink to = '/' className="nav-link"> <img src="/images/home-icon.png" alt="home icon" width="20rem"/> Home </NavLink>
+        <NavLink onClick={handleLogOut} to="/" className="nav-link">Sign Out</NavLink>
+        <NavLink to = '/zodiacs' className="nav-link"> <img src="/images/astrology-icon.png" alt="astrology icon" width="22rem"/> Zodiac Signs </NavLink>
+        <NavLink to = '/profile' className="nav-link"> <img src="/images/profile-icon.png" alt="profile icon" width="20rem"/> Profile </NavLink>
+        <NavLink to = '/about_us' className="nav-link"> About Us </NavLink>
+      </nav>
+    )
+  }
+ 
+  const publicOptions = (
+    <nav>
+      <NavLink to = '/login' className="nav-link"> Login </NavLink>
+    </nav>
+  )
+ 
+ 
   return(  
-    <div className = "nav-container">
-      
+    <header className = "nav-container">
+      {/* Marquee banner */}
       <span id='nav-banner'>
         <marquee> 
           <h4 id='nav-banner-text'>The stars are about to line. Learn about your zodiac sign NOW!!!</h4>
         </marquee>
       </span>
-        
+        {/* Header */}
         <div id='nav-content'>  
           <Link to ={`/`}><img src="https://i.imgur.com/7O8c7S6.png" alt="website logo" className='logo'/></Link>
           <div id='links-container'>
-              <NavLink to = '/' className="nav-link"> <img src="/images/home-icon.png" alt="home icon" width="20rem"/> Home </NavLink>
+          {authenticated && user ? authenticatedOptions : publicOptions}
+
+              {/* <NavLink to = '/' className="nav-link"> <img src="/images/home-icon.png" alt="home icon" width="20rem"/> Home </NavLink>
               <NavLink to = '/login' className="nav-link"> Login </NavLink>
               <NavLink to = '/zodiacs' className="nav-link"> <img src="/images/astrology-icon.png" alt="astrology icon" width="22rem"/> Zodiac Signs </NavLink>
               <NavLink to = '/profile' className="nav-link"> <img src="/images/profile-icon.png" alt="profile icon" width="20rem"/> Profile </NavLink>
-              <NavLink to = '/about_us' className="nav-link"> About Us </NavLink>
+              <NavLink to = '/about_us' className="nav-link"> About Us </NavLink> */}
           </div>
         </div>
     
-    </div>
+    </header>
   )
 }
 
