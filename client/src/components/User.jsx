e8ffc065373e5d40ea0e4b2d76e30a12fbdf9194
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-11-16 13:39:22 -0500

    added link to images in user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const User = ({id, firstName, lastName, zodiacName, profilePic}) => {
   return (
@@ -10,14 +11,19 @@ const User = ({id, firstName, lastName, zodiacName, profilePic}) => {
             <span id="zodiacSign"> <img src="https://i.imgur.com/TkMTiQu.png" alt="astrology icon" width="22rem"/>&nbsp; <h3>{zodiacName}</h3></span>
           </div>
           <div className='icons-container'> 
+            <Link to={`/delete`}>
+            
             <div className='icon-card'> 
               <img src={`https://i.imgur.com/soJ17a8.png`} alt={`red circle`} width="60px" className='circle-icon'/> 
               <img src={`https://i.imgur.com/aOo2Bcv.png`} alt={`x`} width="30px" className='option-icon'/> 
             </div>
+            </Link>
+            <Link to={`/favorite`}> 
             <div className='icon-card'> 
               <img src={`https://i.imgur.com/tk8nt81.png`} alt={`green circle`} width="60px" className='circle-icon'/> 
               <img src={`https://i.imgur.com/0z8S6Uu.png`} alt={`heart`} width="30px" className='option-icon'/> 
             </div>
+            </Link>
           </div>
         </div>
       
